04af3be0e129397fd222f4f2d7c42059d5dc5c51
ubaid ur rehman <110807502+helloword1100@users.noreply.github.com>
2023-10-07 01:30:44 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+const countdown=new Date("Oct 10,2023 00:00:00").getTime();
+
+var x=setInterval(function(){
+    var now=new Date().getTime();
+    var distance=countdown-now;
+    var days=Math.floor(distance/(1000*60*60*24));
+    var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+    var minutes=Math.floor((distance%(1000*60*60))/(1000*60));
+    var seconds=Math.floor((distance%(1000*60))/1000);
+
+    document.getElementById('days').innerHTML=days;
+    document.getElementById('hrs').innerHTML=hours;
+    document.getElementById('min').innerHTML=minutes;
+    document.getElementById('sec').innerHTML=seconds;
+
+
+},1000)
